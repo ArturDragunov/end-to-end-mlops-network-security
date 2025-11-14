693f8a57d3c8d369a7d4ece40744cdaf5645be5c
ArturDragunov <dragunovartur61@gmail.com>
2025-11-14 16:45:25 +0100

    BUG FIXES

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-!bin/sh
-nohup airflow scheduler &
-airflow webserver
+#!/bin/sh
+
+echo "Migrating Airflow database..."
+airflow db migrate
+
+echo "Starting Airflow in standalone mode (includes web UI, scheduler, and default user)..."
+exec airflow standalone
