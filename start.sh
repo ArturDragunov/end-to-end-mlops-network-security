38f98e4f84827f4ca3812cf9bf96908da16875a3
ArturDragunov <dragunovartur61@gmail.com>
2025-11-14 16:20:12 +0100

    rewrite docker

--- start.sh
+++ start.sh
@@ -1,19 +1,3 @@
-#!/bin/sh
-
-echo "Initializing Airflow DB..."
-airflow db migrate
-
-echo "Creating Airflow admin user..."
-airflow users create \
-    --username admin \
-    --password admin \
-    --firstname Artur \
-    --lastname Dragunov \
-    --role Admin \
-    --email dragunovartur61@gmail.com || true
-
-echo "Starting Scheduler..."
-airflow scheduler &
-
-echo "Starting Webserver..."
+!bin/sh
+nohup airflow scheduler &
 airflow webserver
